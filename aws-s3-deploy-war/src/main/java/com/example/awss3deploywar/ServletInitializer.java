79917b8fc670ceab098c20137da08c49ab5e929b
Gautam <gauti783@gmail.com>
2022-01-16 12:13:49 +0530

    added new project

---
+++ aws-s3-deploy-war/src/main/java/com/example/awss3deploywar/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.awss3deploywar;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AwsS3DeployWarApplication.class);
+	}
+
+}
